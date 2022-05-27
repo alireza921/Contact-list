ea9cd3ea173924321ff75c5ee4d154179d4c19bb
AliReza <alirezaesm921@gmail.com>
2022-05-27 12:42:14 +0430

    final edit-save to localStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from "react";
 import { useState } from "react";
 import "./App.css";
 import AddContact from "./components/AddContact/AddContact";
@@ -6,27 +6,40 @@ import ContactList from "./components/ContactList/contactList";
 
 function App() {
   const [contacts, setContacts] = useState([]);
-
+  
   const onAddContact = (addContact) => {
     setContacts([
       ...contacts,
       { ...addContact, id: Math.floor(Math.random() * 1000) },
     ]);
-
+    
     // console.log(contacts);
   };
-
+  
   const deleteContactHandler = (id) => {
     console.log("delet Contact ", id);
     const filteredContact = contacts.filter((c) => c.id !== id);
     setContacts(filteredContact);
   };
+  
+  
+    useEffect(() => {
+      const savedContacts = JSON.parse(localStorage.getItem("contacts"));
+      if (savedContacts) {
+        setContacts(savedContacts);
+      }
+    }, []);
+  
+    useEffect(() => {
+      localStorage.setItem("contacts", JSON.stringify(contacts));
+    }, [contacts]);
 
+    
   return (
     <div className='App'>
       <h1> contacts App </h1>
-        <AddContact onAddContact={onAddContact} />
-        <ContactList contacts={contacts} onDelete={deleteContactHandler} />
+      <AddContact onAddContact={onAddContact} />
+      <ContactList contacts={contacts} onDelete={deleteContactHandler} />
     </div>
   );
 }
